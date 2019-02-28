66f81151c5403a63f11ee8dd5b3d24aabb42471a
Damian Tomasila <DamianTomasila@hotmail.com>
2019-02-28 15:12:52 +0100

    Hexagon grootte aangepast ;)

--- Catan Asp._Net/Views/Catan/HexagonPosition.cs
+++ Catan Asp._Net/Views/Catan/HexagonPosition.cs
@@ -15,52 +15,52 @@ namespace Catan_Asp._Net.Views.Catan
         public int margin_leftnumber;
         public void Margin_Left(int i, int margin_left)
         {
-            margin_left += 104; //leave 104px between all tiles
+            margin_left += 107; //leave 104px between all tiles
             switch (i)
             {
                 case 4:
-                    { margin_left = 254; }   //2nd row
+                    { margin_left = 257; }  //2nd row
                     break;
                 case 9:
-                    { margin_left = 202; }  //3rd row
+                    { margin_left = 203; }  //3rd row
                     break;
                 case 15:
                     { margin_left = 150; }  //4th row
                     break;
                 case 22:
-                    { margin_left = 202; }  //5th row
+                    { margin_left = 203; }  //5th row
                     break;
                 case 28:
-                    { margin_left = 254; } //6th row
+                    { margin_left = 257; } //6th row
                     break;
                 case 33:
-                    { margin_left = 306; }  //7th row
+                    { margin_left = 311; }  //7th row
                     break;
             }
             new_margin_left = margin_left;
-            margin_leftnumber = margin_left + 22;
+            margin_leftnumber = margin_left + 20;
         }
         public void Margin_Top(int i, int margin_top)
         {
             switch (i)
             {
                 case 4:
-                    { margin_top = 21; }   //2nd row
+                    { margin_top = 31; }   //2nd row
                     break;
                 case 9:
-                    { margin_top = 104; }  //3rd row
+                    { margin_top = 124; }  //3rd row
                     break;
                 case 15:
-                    { margin_top = 187; }  //4th row
+                    { margin_top = 217; }  //4th row
                     break;
                 case 22:
-                    { margin_top = 270; }  //5th row
+                    { margin_top = 310; }  //5th row
                     break;
                 case 28:
-                    { margin_top = 353; } //6th row
+                    { margin_top = 403; } //6th row
                     break;
                 case 33:
-                    { margin_top = 436; }  //7th row
+                    { margin_top = 496; }  //7th row
                     break;
             }
             new_margin_top = margin_top;
