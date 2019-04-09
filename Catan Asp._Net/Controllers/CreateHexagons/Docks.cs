0f5b46cce2a35133a5cce120a47df5e72f1bbbb8
tjerk <tjerk@LAPTOP-J9UM9MOF>
2019-04-09 09:22:48 +0200

    Dockd docktype path toegevoegd

--- Catan Asp._Net/Controllers/CreateHexagons/Docks.cs
+++ Catan Asp._Net/Controllers/CreateHexagons/Docks.cs
@@ -9,7 +9,6 @@ namespace Catan_Asp._Net.Controllers.CreateHexagons
     public class Docks
     {
         int randomDock;
-        string trade;
         int woodDock = 1;
         int stoneDock = 1;
         int sheepDock = 1;
@@ -31,39 +30,41 @@ namespace Catan_Asp._Net.Controllers.CreateHexagons
             Random random = new Random();
             randomDock = random.Next(1, 7);
 
+            string HarborKind = "";
             switch(randomDock)
             {
                 case 1:
                 if (woodDock < 1) { randomDock = 1; goto case 2; }
-                if (woodDock == 1) { trade = "../Images/WoodDock.png"; woodDock--; }
+                if (woodDock == 1) { HarborKind = "WoodDock"; woodDock--; }
                      break;
 
                 case 2:
                 if (stoneDock < 1) { randomDock = 2; goto case 3; }
-                if (stoneDock == 1) { trade = "../Images/StoneDock.png"; stoneDock--; }
+                if (stoneDock == 1) { HarborKind = "StoneDock"; stoneDock--; }
                     break;
 
                 case 3:
                 if (sheepDock < 1) { randomDock = 3; goto case 4; }
-                if (sheepDock == 1) { trade = "../Images/SheepDock.png"; sheepDock--; }
+                if (sheepDock == 1) { HarborKind = "SheepDock"; sheepDock--; }
                     break;
 
                 case 4:
                 if (grainDock < 1) { randomDock = 4; goto case 5; }
-                if (grainDock == 1) { trade = "../Images/GrainDock.png"; grainDock--; }
+                if (grainDock == 1) { HarborKind = "GrainDock"; grainDock--; }
                     break;
 
                 case 5:
                 if (oreDock < 1) { randomDock = 5; goto case 6; }
-                if (oreDock == 1) { trade = "../Images/OreDock.png"; oreDock--; }
+                if (oreDock == 1) { HarborKind = "OreDock"; oreDock--; }
                     break;
 
                 case 6:
                 if (generalDock == 0) { randomDock = 6; goto case 1; }
-                if (generalDock > 0) { trade = "../Images/GeneralDock.png"; generalDock--; }
+                if (generalDock > 0) { HarborKind = "GeneralDock"; generalDock--; }
                     break;
             }
-            return trade;
+            string path = $"../Images/{HarborKind}.png";
+            return path;
         }
 
         public DockLines dockLine(int i, int count)
