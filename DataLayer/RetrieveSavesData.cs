acbc18c0a94a467d9b775ab77cd1f4c61915b75d
tjerk <tjerk@LAPTOP-J9UM9MOF>
2019-04-25 10:51:05 +0200

    adsada

--- DataLayer/RetrieveSavesData.cs
+++ DataLayer/RetrieveSavesData.cs
@@ -16,32 +16,32 @@ namespace DataLayer
             List<Save> saves = new List<Save>();
             try
             {
-                SqlConnection conn = new SqlConnection(
-                    @"Server=mssql.fhict.local;
-                    Database=dbi386599;
-                    User Id=dbi386599;
-                    Password=12345;"
-                 );
-                using (conn)
-                {
-                    conn.Open();
-                    string query = "SELECT * FROM [Saves]";
-                    SqlCommand cmd = new SqlCommand(query, conn);
-                    DataTable dtResult = new DataTable();
-                    dtResult.Load(cmd.ExecuteReader());
-                    conn.Close();
-                    foreach (DataRow dr in dtResult.Rows)
-                    {
-                        Save save = new Save();
+                //SqlConnection conn = new SqlConnection(
+                //    @"Server=mssql.fhict.local;
+                //    Database=dbi386599;
+                //    User Id=dbi386599;
+                //    Password=12345;"
+                // );
+                //using (conn)
+                //{
+                //    conn.Open();
+                //    string query = "SELECT * FROM [Saves]";
+                //    SqlCommand cmd = new SqlCommand(query, conn);
+                //    DataTable dtResult = new DataTable();
+                //    dtResult.Load(cmd.ExecuteReader());
+                //    conn.Close();
+                //    foreach (DataRow dr in dtResult.Rows)
+                //    {
+                //        Save save = new Save();
 
-                        save.Name = dr[1].ToString();
+                //        save.Name = dr[1].ToString();
 
-                        DateTime.TryParse(dr[2].ToString(), out DateTime Time);
-                        save.Time = Time;
+                //        DateTime.TryParse(dr[2].ToString(), out DateTime Time);
+                //        save.Time = Time;
 
-                        saves.Add(save);
-                    }
-                }
+                //        saves.Add(save);
+                //    }
+                //}
             }
             catch (Exception e)
             {
